310a9df9ba5f8bf36252d7fd8ede0778ef46efcc
John Athayde <jmpa@meticulous.com>
2021-07-14 22:32:28 -0400

    Resume page placeholder, updating routes

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,7 @@
 class PagesController < ApplicationController
   def index
   end
+
+  def resume
+  end
 end
--- spec/requests/pages_spec.rb
+++ spec/requests/pages_spec.rb
@@ -1,9 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe "Pages", type: :request do
-  describe "GET /index" do
+  describe "GET root index" do
     it "returns http success" do
-      get "/pages/index"
+      get "/"
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "GET /resume" do
+    it "returns http success" do
+      get "/resume"
       expect(response).to have_http_status(:success)
     end
   end
