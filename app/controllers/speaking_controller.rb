1ccb13d7e1ad2d1ac8a95e7701efdc36decbd32e
John Athayde <jmpa@meticulous.com>
2022-09-10 16:11:09 -0400

    Finishing Speaking Index

--- app/controllers/speaking_controller.rb
+++ app/controllers/speaking_controller.rb
@@ -2,5 +2,6 @@ class SpeakingController < ApplicationController
   def index
     @appearances = Appearance.upcoming
     @past_appearances = Appearance.past.order(date: :desc).limit(5)
+    @talks = Talk.all.order(id: :desc)
   end
 end
